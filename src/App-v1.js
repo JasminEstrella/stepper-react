540ca261423990ee36af1de90bcef68e5370434f
Jaja <jestrella@pacificseainvests.com>
2024-11-06 16:27:36 +0800

    Apply reusable button with children prop

--- src/App-v1.js
+++ src/App-v1.js
@@ -10,7 +10,8 @@ export default function App() {
   return (
     <div>
       <Steps></Steps>
-      {/* <Steps></Steps> */}
+      <Steps></Steps>
+      <Steps></Steps>
     </div>
   )
 }
@@ -44,17 +45,11 @@ function Steps() {
         </p>
 
         <div className="buttons">
-          <Button bgColor="#7950F2" textColor="#fff" onClick={handlePrevious}><span>👈🏻</span>Previous</Button>
-          <Button bgColor="#7950F2" textColor="#fff" onClick={handleNext}>Next<span>👉🏻</span></Button>
+          <button style={ step === 1 ? {backgroundColor: 'grey', color: '#fff' } : {backgroundColor: '#7950F2', color: '#fff' }} onClick={ handlePrevious } >Previous</button>
+          <button style={step === 3 ? {backgroundColor: 'grey', color: '#fff' } : {backgroundColor: '#7950F2', color: '#fff'}} onClick={ handleNext }>Next</button>
         </div>
       </div>
     )}
     </div>
-  )
-}
-
-function Button({bgColor, textColor, onClick, children}) {
-  return (
-    <button style={{backgroundColor: bgColor, color: textColor }} onClick={ onClick }> {children} </button>
   )
 }
